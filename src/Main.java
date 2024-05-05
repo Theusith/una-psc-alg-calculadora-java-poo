0a399698e52396309df8e39e4d47afcb8c1b23e7
theusith <theussith@hotmail.com>
2024-05-05 19:44:14 -0300

    Atividade concluida!

--- src/Main.java
+++ src/Main.java
@@ -4,42 +4,76 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Calculadora calc = new Calculadora();
-        calc.menu();
-        String operacao = sc.next();
+        String operacao = "1";
 
-        while (operacao.equalsIgnoreCase("q")){
+
+        while (!operacao.equalsIgnoreCase("q")){
             calc.menu();
-            int a = sc.nextInt();
-            int b = sc.nextInt();
+            operacao = sc.next();
+            int a;
+            int b;
             switch (operacao){
                 case "1":
-                    System.out.println(calc.soma(a, b));
+                    System.out.println("Digite um numero: ");
+                    a = sc.nextInt();
+                    System.out.println("Digite um numero: ");
+                    b = sc.nextInt();
+                    System.out.println("Resultado: " + calc.soma(a, b));
                     break;
+
                 case "2":
-                    System.out.println(calc.subtracao(a, b));
+                    System.out.println("Digite um numero: ");
+                    a = sc.nextInt();
+                    System.out.println("Digite um numero: ");
+                    b = sc.nextInt();
+                    System.out.println("Resultado: " + calc.subtracao(a, b));
                     break;
+
                 case "3":
-                    System.out.println(calc.multiplicacao(a, b));
+                    System.out.println("Digite um numero: ");
+                    a = sc.nextInt();
+                    System.out.println("Digite um numero: ");
+                    b = sc.nextInt();
+                    System.out.println("Resultado: " + calc.multiplicacao(a, b));
+                    break;
+
                 case "4":
+                    System.out.println("Digite o numerador: ");
+                    a = sc.nextInt();
+                    System.out.println("Digite o denominador: ");
+                    b = sc.nextInt();
                     if(b == 0){
                         System.out.println("Impossível realizar uma divisão por Zero");
-                    }else System.out.println(calc.divisao(a, b));
+                    }else System.out.println("Resultado: " + calc.divisao(a, b));
                     break;
+
                 case "5":
-                    System.out.println(calc.potencia(a, b));
+                    System.out.println("Digite um numero: ");
+                    a = sc.nextInt();
+                    System.out.println("Digite a potencia: ");
+                    b = sc.nextInt();
+                    if(b == 0){
+                        System.out.println("Todo numro elevado a zero é zero!");
+                    }else System.out.println("Resultado: " + calc.potencia(a, b));
                     break;
+
                 case "6":
-                    System.out.println(calc.raiz(a));
+                    System.out.println("Digite um numero: ");
+                    a = sc.nextInt();
+                    if(a < 0){
+                        System.out.println("Não existe raiz de numero negativo!");
+                    }else System.out.println("Resultado: " + calc.raiz(a));
                     break;
-                case "7":
+
+                case "q":
                     System.out.println("Adeus!");
                     break;
+
                 default:
                     System.out.println("Opcão inválida, tente novamente!");
+                    break;
             }
-            operacao = sc.next();
+            System.out.println("----------------------------------------");
         }
-
-
     }
 }
